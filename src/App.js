6d42f757259adaa7d3563256dbab0e1ecc0b9805
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-09 17:08:20 -0700

    render dog image from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [dogs, setDogs] = useState({});
+  const [dogURL, setDogURL] = useState("");
+
+  useEffect(() => {
+    axios.get(`https://api.thedogapi.com/v1/images/search`).then((res) => {
+      const newDogs = res.data;
+      setDogs(newDogs);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (dogs[0] != null) {
+      setDogURL(dogs[0].url);
+    }
+  }, [dogs]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dogURL} className="App-logo" alt="logo" />
+        <p>
+          <code>Refresh for a new dog.</code>
+        </p>
+      </header>
+    </div>
+    // <div className="App App-header">
+    //   <img width="400px" src={dogURL} alt="dog"></img>
+    // </div>
+  );
 }
 
 export default App;
