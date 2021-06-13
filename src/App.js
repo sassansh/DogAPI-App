dc118e380fef576be08016dc0a7296b46c243028
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-13 12:40:36 -0700

    📦 NEW: New Dog button & social images

--- src/App.js
+++ src/App.js
@@ -5,12 +5,42 @@ import { useEffect, useState } from "react";
 function App() {
   const [dogs, setDogs] = useState({});
   const [dogURL, setDogURL] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+  function getDog() {
+    setLoading(true);
+    axios
+      .get(`https://api.thedogapi.com/v1/images/search`)
+      .then((res) => {
+        const newDogs = res.data;
+        setDogs(newDogs);
+      })
+      .finally(() => {
+        sleep(500);
+        setLoading(false);
+      });
+  }
 
   useEffect(() => {
-    axios.get(`https://api.thedogapi.com/v1/images/search`).then((res) => {
-      const newDogs = res.data;
-      setDogs(newDogs);
-    });
+    setLoading(true);
+    axios
+      .get(`https://api.thedogapi.com/v1/images/search`)
+      .then((res) => {
+        const newDogs = res.data;
+        setDogs(newDogs);
+      })
+      .finally(() => {
+        sleep(500);
+        setLoading(false);
+      });
   }, []);
 
   useEffect(() => {
@@ -19,13 +49,28 @@ function App() {
     }
   }, [dogs]);
 
+  let button;
+  if (loading) {
+    button = (
+      <button className="buttonload disabled not-allowed">
+        <i className="fa fa-refresh fa-spin"></i>
+        <code> Loading</code>
+      </button>
+    );
+  } else {
+    button = (
+      <button className="buttonload pointer" onClick={getDog}>
+        <i className="fa fa-refresh"></i>
+        <code> New Dog</code>
+      </button>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={dogURL} className="App-logo" alt="logo" />
-        <p>
-          <code>Refresh for a new dog.</code>
-        </p>
+        <img src={dogURL} className="dog" alt="dog" />
+        <p>{button}</p>
       </header>
     </div>
     // <div className="App App-header">
